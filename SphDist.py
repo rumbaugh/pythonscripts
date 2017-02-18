c1f86e633d060b25225cb9a618c01b306bfd3e5e
Nick Rumbaugh <takkyon13@gmail.com>
2017-02-18 11:28:13 -0800

    vingilot - Sat Feb 18 11:28:13 PST 2017

--- SphDist.py
+++ SphDist.py
@@ -1,1 +1,7 @@
-../SphDist.py
+import numpy as np
+
+def SphDist(RAi,Deci,Raf,Decf):
+	#Distance in arcminutes. Uses Haversine Formula
+	dist = 2.0*np.arcsin(np.sqrt((np.sin(0.5*np.pi*(Decf-Deci)/180))**2+np.cos(np.pi*Decf/180.0)*np.cos(np.pi*Deci/180.0)*(np.sin(0.5*np.pi*(Raf-RAi)/180))**2))
+	dist *= 180.0*60/np.pi
+	return dist
